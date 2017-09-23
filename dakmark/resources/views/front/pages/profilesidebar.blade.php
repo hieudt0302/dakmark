b9a4384d061a553cddc388d57d419f6543faf03b
bravohex <bravohex@outlook.com>
2017-09-23 15:06:55 +0700

    clear source

--- dakmark/resources/views/front/pages/profilesidebar.blade.php
+++
@@ -1,7 +0,0 @@
- <div>
-    <ul class="list-group">
-        <li class="list-group-item"><a href="{{ url('/profile') }}">Thông Tin</a></li>
-        <li class="list-group-item"><a href="{{ url('/profile/address') }}">Sổ Địa Chỉ</a></li>
-        <li class="list-group-item"><a href="{{ url('/profile/orders') }}">Đơn Đặt Hàng</a></li>
-    </ul>
-</div>
