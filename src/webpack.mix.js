2d9f6e9b00fd60ff4cfa0cdd1281674150fca483
bravohex <bravohex@outlook.com>
2017-09-26 22:42:29 +0700

    setup base assets

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,10 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//     .sass('resources/assets/sass/app.scss', 'public/css')
+
+mix.js('resources/assets/rhythm/js/all.js', 'public/js')
+    // mix.copyDirectory('resources/assets/rhythm/js', 'public/js');
+mix.copyDirectory('resources/assets/rhythm/css', 'public/css')
+mix.copyDirectory('resources/assets/rhythm/fonts', 'public/fonts');
