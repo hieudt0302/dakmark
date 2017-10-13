99e42bec91b2d31c5ec21a7d399436f0fb21eea1
bravohex <bravohex@outlook.com>
2017-10-14 00:37:37 +0700

    Update admin orders index

---
+++ src/resources/lang/en/status.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'order' => [
+        1 => 'Pending',
+        2 => 'Processing',
+        3 => 'Complete',
+        4 => 'Cancelled'
+    ],
+
+    'shipping' => [
+        1 => 'Shipping Not Required',
+        2 => 'Not Yet Shipped',
+        3 => 'Partially Shipped',
+        4 => 'Shipped',
+        5 => 'Delivered',
+    ],
+
+    'payment' => [
+        1 => 'Pending',
+        2 => 'Authorized',
+        3 => 'Paid',
+        4 => 'Partially Refunded',
+        5 => 'Refunded',
+        6 => 'Voided',
+    ]
+];
