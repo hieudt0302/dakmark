81fe8b96da3b1a5704fc63fd3ba1ed4375dd5953
Lê Minh Tuấn <lmt.register@outlook.com>
2018-06-15 14:20:44 +0700

    Update BannersController.php

--- src/app/Http/Controllers/Admin/BannersController.php
+++ src/app/Http/Controllers/Admin/BannersController.php
@@ -35,29 +35,29 @@ class BannersController extends Controller
             $banner = $request->file('banner-left');            
             $img = Image::make($banner->getRealPath());
             $img->fit(960, 400)
-               //->save('frontend/images/uploads/ads-bg1.jpg');                      
-               ->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
+               ->save('frontend/images/uploads/ads-bg1.jpg');                      
+               //->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
         } 
         if (request()->hasFile('banner-right')) {
             $banner = $request->file('banner-right');            
             $img = Image::make($banner->getRealPath());
             $img->fit(960, 400)
-               //->save('frontend/images/uploads/ads-bg2.jpg');                      
-              ->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
+               ->save('frontend/images/uploads/ads-bg2.jpg');                      
+              //->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
         } 
         if (request()->hasFile('banner-full')) {
             $banner = $request->file('banner-full');            
             $img = Image::make($banner->getRealPath());
             $img->fit(1920, 400)
-               //->save('frontend/images/uploads/ads-bg3.jpg');                      
-               ->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
+               ->save('frontend/images/uploads/ads-bg3.jpg');                      
+               //->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
         }
         if (request()->hasFile('banner-blog')) {
             $banner = $request->file('banner-blog');            
             $img = Image::make($banner->getRealPath());
             $img->fit(270, 370)
-               //->save('frontend/images/uploads/sale.jpg');                      
-               ->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
+               ->save('frontend/images/uploads/sale.jpg');                      
+               //->save(public_path('images/uploads/'. $banner->getClientOriginalName()));
         }          
 
         //Icon
@@ -66,32 +66,32 @@ class BannersController extends Controller
             $img = Image::make($icon->getRealPath());
             $img->resize(null, 120, function ($constraint) {
                 $constraint->aspectRatio();
-            //})->save('images/logo/poko.png');    
-               })->save(public_path('images/logo/'. $icon->getClientOriginalName()));
+            })->save('images/logo/poko.png');    
+               //})->save(public_path('images/logo/'. $icon->getClientOriginalName()));
         }
         if (request()->hasFile('icon-2')) {
             $icon = $request->file('icon-2');            
             $img = Image::make($icon->getRealPath());
             $img->resize(null, 120, function ($constraint) {
                 $constraint->aspectRatio();
-            //})->save('images/logo/origin.png'); 
-               })->save(public_path('images/logo/'. $icon->getClientOriginalName()));
+            })->save('images/logo/origin.png'); 
+               //})->save(public_path('images/logo/'. $icon->getClientOriginalName()));
         }
         if (request()->hasFile('icon-3')) {
             $icon = $request->file('icon-3');            
             $img = Image::make($icon->getRealPath());
             $img->resize(null, 120, function ($constraint) {
                 $constraint->aspectRatio();
-            //})->save('images/logo/fairtrade.png');                      
-            })->save(public_path('images/logo/'. $icon->getClientOriginalName()));
+            })->save('images/logo/fairtrade.png');                      
+            //})->save(public_path('images/logo/'. $icon->getClientOriginalName()));
         }    
         if (request()->hasFile('icon-4')) {
             $icon = $request->file('icon-4');            
             $img = Image::make($icon->getRealPath());
             $img->resize(null, 120, function ($constraint) {
                 $constraint->aspectRatio();
-            //})->save('images/logo/community-icon.png');      
-               })->save(public_path('images/logo/'. $icon->getClientOriginalName()));
+            })->save('images/logo/community-icon.png');      
+               //})->save(public_path('images/logo/'. $icon->getClientOriginalName()));
         }                                                 
       
         return redirect()->back()
