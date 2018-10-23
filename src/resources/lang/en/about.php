ca895eecf6b5a1d15cf3e670638a140445d07f7a
Lê Minh Tuấn <lmt.register@outlook.com>
2018-10-23 15:42:28 +0700

    Update about.php

--- src/resources/lang/en/about.php
+++ src/resources/lang/en/about.php
@@ -3,7 +3,9 @@
 return [
     'our-vision' => 'Our vision',
     'our-mission' => 'Our mission',
-    'vision-details' => 'Pô Kô Farms determines to become a sustainable development cooperative, that helps farmers to have a better life through responsible coffee producing. Furthermore, Pô Kô Farms will be the first choice of customers who want to buy Fairtrade certified coffee in particular and agricutural products in general from Vietnam.',
-    'mission-details' => 'Pô Kô Farms aims to become a cooperative that specializes in producing and supplying high quality Fairtrade certified coffee for current buyers and new customers in the future. By growing and selling Fairtrade certified coffee, Pô Kô Farms wishes to improve farmers income and quality of life for their households . Sharing together and developing together, make the world better with quality coffee.',                    
+    'vision-details' => 'Pô Kô Farms determines to become a sustainable development cooperative, that helps farmers to have a better life through responsible coffee producing. Furthermore, Pô Kô Farms always tries best to become the first choice of customers who want to buy Fairtrade certified coffee in particular and agricutural  products in general from Vietnam.',
+    'mission-details' => 'Pô Kô Farms aims to become a cooperative that specializes in producing and supplying high quality Fairtrade certified coffee as well as agricultural products for current buyers and new customers in the future.<br>
+By growing and selling Fairtrade certified coffee, Pô Kô Farms wishes to improve farmers income and quality of life for their  households. Additinally, Pô Kô Farms aims to create livelihoods and inspire future generations to love developing themselves in coffee sector.<br>
+Sharing together and developing together, make the world better with quality coffee.',                    
 
 ];
