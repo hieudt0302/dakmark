aaa985653ba0e5e90bca36ca1c6a763ab228ae84
Lê Minh Tuấn <lmt.register@outlook.com>
2018-10-23 15:41:33 +0700

    Update about.php

--- src/resources/lang/vi/about.php
+++ src/resources/lang/vi/about.php
@@ -3,7 +3,9 @@
 return [
     'our-vision' => 'Tầm nhìn',
     'our-mission' => 'Sứ mệnh ',
-    'vision-details' => 'Pô Kô Farms quyết tâm trở thành hợp tác xã phát triển bền vững, giúp người nông dân có cuộc sống tốt đẹp hơn từ việc sản xuất cà phê có trách nhiệm. Đồng thời, Pô Kô farms sẽ là lựa chọn uy tín hàng đầu của khách hàng khi mua sản phẩm cà phê nói riêng và nông sản nói chung được chứng nhận Fairtrade tại Việt Nam.',
-    'mission-details' => 'Pô Kô Farms hướng tới trở thành một hợp tác xã chuyên sản xuất và cung cấp cà phê chứng nhận Fairtrade chất lượng cao cho khách hàng hiện tại và khách hàng mới trong tương lai. Thông qua việc trồng và bán cà phê chứng nhận Fairtrade, Pô Kô Farms mong muốn có thể cải thiện thu nhập và chất lượng sống của các nông hộ thành viên và gia đình họ. Cùng nhau chia sẻ và cùng nhau phát triển, làm cho thế giới tốt đẹp hơn từ những hạt cà phê chất lượng.',                    
+    'vision-details' => 'Pô Kô Farms phấn đấu trở thành hợp tác xã cung cấp nông sản chứng nhận Fairtrade uy tín, xây dựng thành công chuỗi sản phẩm và dịch vụ liên quan,góp phần cải thiện cuộc sống của người nông dân bắt đầu từ việc sản xuất cà phê có trách nhiệm.',
+    'mission-details' => 'Xây dựng thành công thương hiệu nông sản đạt chứng nhận Fairtrade của nông dân Đắk Hà bắt đầu từ xây dựng thương hiệu cà phê Robusta chất lượng cao mang đặc trưng của vùng Đắk Hà.<br>
+Thông qua việc trồng và bán cà phê chất lượng cao, đạt chứng nhận Fairtrade, Pô Kô Farms mong muốn cải thiện thu nhập và chất lượng cuộc sống của các nông hộ thành viên và gia đình họ. Đồng thời, Pô Kô Farms hướng tới tạo sinh kế và truyền cảm hứng cho thế hệ tiếp nối yêu thích việc phát triển trong lĩnh vực sản xuất cà phê nói riêng và nông nghiệp nói chung.<br>
+Cùng nhau chia sẻ và cùng nhau phát triển, làm cho thế giới tốt đẹp hơn từ những hạt cà phê chất lượng.',                    
 
 ];
