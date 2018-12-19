c0468bc678508fde94c22a2c88b3e323544a4a1f
Lê Minh Tuấn <lmt.register@outlook.com>
2018-12-19 22:18:09 +0700

    Update index.blade.php

--- src/resources/views/front/products/index.blade.php
+++ src/resources/views/front/products/index.blade.php
@@ -4,7 +4,7 @@
 <!-- Share Nav -->
 @include('layouts.share')
 <style>
-.search-form div input{
+  .search-form div input{
     padding: 12px;
     /* border: none; */
     font-family: "Lato", 'Source Sans Pro', serif;
@@ -37,98 +37,100 @@
 @section('content')
 
 <div class="hero">
-    <div class="container">
-        @if(!empty($promo)&&$promo==true)
-        <div class="row">
-            <div class="col-md-12">
-                <h1>@lang('header.promotion')</h1>
-                <ul class="breadcumb">
-                    <li><a href="{{ Setting::config('website')}}">@lang('common.home')</a></li>
-                    <li><span>/</span>@lang('product.product')</li>
-                    <li><span>/</span>@lang('header.promotion')</li>
-                </ul>
-            </div>
-        </div>
-        @else
-        <div class="row">
-            <div class="col-md-12">
-                <h1>{{$category->translation->name??__('product.products')}}</h1>
-                <ul class="breadcumb">
-                    <li><a href="{{ Setting::config('website')}}">@lang('common.home')</a></li>
-                    <li><span>/</span>@lang('product.product')</li>
-                    <li><span>/</span>{{$category->translation->name??__('product.products')}}</li>
-                </ul>
-            </div>
-        </div>
-        @endif
+  <div class="container">
+    @if(!empty($promo)&&$promo==true)
+    <div class="row">
+      <div class="col-md-12">
+        <h1>@lang('header.promotion')</h1>
+        <ul class="breadcumb">
+          <li><a href="{{ Setting::config('website')}}">@lang('common.home')</a></li>
+          <li><span>/</span>@lang('product.product')</li>
+          <li><span>/</span>@lang('header.promotion')</li>
+        </ul>
+      </div>
     </div>
+    @else
+    <div class="row">
+      <div class="col-md-12">
+        <h1>{{$category->translation->name??__('product.products')}}</h1>
+        <ul class="breadcumb">
+          <li><a href="{{ Setting::config('website')}}">@lang('common.home')</a></li>
+          <li><span>/</span>@lang('product.product')</li>
+          <li><span>/</span>{{$category->translation->name??__('product.products')}}</li>
+        </ul>
+      </div>
+    </div>
+    @endif
+  </div>
 </div>
 
 <section class="shopgrid products">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 col-sm-6 col-xs-12">
-				<div class="left-it">
-					<h5>@lang('common.showing') <span class="sub">{{($results->currentPage()- 1) * 21 + 1}}-{{$results->total() * $results->currentPage() }} @lang('common.of') {{$results->count()}}</span> </h5>
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-6 col-xs-12">
-				<div class="right-it">
-				</div>
-			</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="left-it">
+          <h5>@lang('common.showing') <span class="sub">{{($results->currentPage()- 1) * 21 + 1}}-{{$results->total() *
+              $results->currentPage() }} @lang('common.of') {{$results->count()}}</span> </h5>
         </div>
-        @php($curDate = Carbon\Carbon::now())
-        @php($index = 0)
-        @foreach($results as $product)
-            @php($isSale = 0)
-            @if($product->special_price != 0 && $product->special_price_start_date  <= $curDate && $curDate <= $product->special_price_end_date )
-                @php($isSale = 1)
-            @endif          
-            @if($index == 0 || $index % 4 == 0)
-            <div class="row">
-                <div class="products-it">
-            @endif                    
-                <div class="col-md-3 col-sm-6 col-xs-12">
-                    <div class="pro-it">
-                        <a href="{{url('/products')}}/{{$product->slug}}">
-                        <img class="pro-img" src="{{asset('/storage')}}/{{$product->GetMediaByOrderAsc()->thumb??'images/no-image.png'}}" alt="">
-                        </a>
-
-			
-                        <div class="pro-infor tuan-fix-test">
-                            @if ($isSale == 1)
-                            <p><span class="hot">@lang('product.sale')</span></p>
-                            @endif                              
-                            <h2>{{$product->translation->name??$product->name}}</h2>
-			    @if(!$product->call_for_price)
-                            @if ($isSale == 1)
-                            <del class="section-text">{{FormatPrice::price($product->price)}}</del> &nbsp;
-                            <strong>{{FormatPrice::price($product->special_price)}}</strong>
-                            @else
-                                <strong>{{FormatPrice::price($product->price)}}</strong>
-                            @endif
-			    @endif
-                        </div>
-			
-                    </div>
+      </div>
+      <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="right-it">
+        </div>
+      </div>
+    </div>
+    @php($curDate = Carbon\Carbon::now())
+    @php($index = 0)
+    <div class="row">
+      <div class="products-it">
+    @foreach($results as $product)
+      {{-- IS SALE --}}
+      @php($isSale = 0)
+      @if($product->special_price != 0 && $product->special_price_start_date <= $curDate && $curDate <=$product->special_price_end_date )
+        @php($isSale = 1)
+      @endif
+        {{-- @if($index == 0 || $index % 4 == 0)
+        <div class="row">
+          <div class="products-it">
+        @endif --}}
+            {{-- BEGIN P --}}
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              <div class="pro-it">
+                <a href="{{url('/products')}}/{{$product->slug}}">
+                  <img class="pro-img" src="{{asset('/storage')}}/{{$product->GetMediaByOrderAsc()->thumb??'images/no-image.png'}}"  alt="No Image">
+                </a>
+                <div class="pro-infor tuan-fix-test">
+                  @if ($isSale == 1)
+                  <p><span class="hot">@lang('product.sale')</span></p>
+                  @endif
+                  <h2>{{$product->translation->name??$product->name}}</h2>
+                  @if(!$product->call_for_price)
+                  @if ($isSale == 1)
+                  <del class="section-text">{{FormatPrice::price($product->price)}}</del> &nbsp;
+                  <strong>{{FormatPrice::price($product->special_price)}}</strong>
+                  @else
+                  <strong>{{FormatPrice::price($product->price)}}</strong>
+                  @endif
+                  @endif
                 </div>
-            @if(($index > 0 && $index % 3 === 0) || $index + 1 === count($results))
-                </div>
+              </div>
             </div>
-            @endif
-        @endforeach
-		
-        
-		<div class="row">
-			<div class="blogpanigation">
-				<div class="col-md-12 col-sm-12 col-xs-12">
-                    {{ $results->links() }}
-				</div>
-			</div>
-		</div>
-	</div>
+            {{-- END P --}}
+        {{-- @if(($index > 0 && $index % 3 === 0) || $index + 1 === count($results))
+          </div>
+        </div>
+        @endif --}}
+    @endforeach
+  </div>
+</div>
+    <div class="row">
+      <div class="blogpanigation">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          {{ $results->links() }}
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
-
 @endsection
 @section('scripts')
 
@@ -136,8 +138,10 @@
 <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script type="text/javascript" src="{{ asset('js/flytocart.js') }}"></script>
 <script>
-{{-- 
-     $(document).ready(function(){      
+  {
+    {
+      --
+      $(document).ready(function () {
         // $('.add-shoopingcart').click(function() {
         //     var id = $("input[name='product_id']").val();
         //     var name = $("input[name='product_name']").val();
@@ -145,7 +149,7 @@
         //     var quantity = 1;//$("input[name='quantity']").val();
         //     $.ajax({
         //        type:'POST',
-        //        url:'{{ url("/add-to-cart") }}',              
+        //        url:'{{ url("/add-to-cart") }}',
         //        data: {
         //             'id': id, //just test
         //             'name': name,//just test
@@ -160,7 +164,9 @@
         //        }
         //     });
         // });
-    });
-    --}}
+      });
+      --
+    }
+  }
 </script>
 @endsection
