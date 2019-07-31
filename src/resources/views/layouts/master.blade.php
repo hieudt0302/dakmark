b0f0ae5c9ad0cf0bf416239972c6822cb0085a43
Lê Khánh Tùng <toilatung@icloud.com>
2019-07-31 23:42:27 +0700

    Google Font

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -17,7 +17,6 @@
 	<meta name="author" content="">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="profile" href="#">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=vietnamese" rel="stylesheet">
 	<!-- <link rel="shortcut icon" href="images/favicon.ico"> -->
 	<!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:bold,black,regular"> -->
 	<!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Playfair+Display:bold,italic,bolditalic"> -->
@@ -25,7 +24,10 @@
 	<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700&amp;subset=cyrillic,vietnamese" rel="stylesheet">	 -->
 	<!-- <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900" > -->
 	<!-- <link  rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,700,900"> -->
-	<!-- -->
+	<!-- 
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=vietnamese" rel="stylesheet">
+	
+	-->
 	<link rel="stylesheet" type="text/css" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 	<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<!-- Mobile specific meta -->
@@ -65,6 +67,43 @@
 
 	@yield('header')
 	<meta name="google-site-verification" content="SBNKsjbUQHBBai8E7O5TxOH4R1pHOiD1F2qaSZbPxBk" />
+	<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com -->
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+	<link rel="preload" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=vietnamese" as="fetch" crossorigin="anonymous">
+	<script type="text/javascript">
+		! function(e, n, t) {
+			"use strict";
+			var o = "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap&subset=vietnamese",
+				r = "__3perf_googleFonts_d63f5";
+
+			function c(e) {
+				(n.head || n.body).appendChild(e)
+			}
+
+			function a() {
+				var e = n.createElement("link");
+				e.href = o, e.rel = "stylesheet", c(e)
+			}
+
+			function f(e) {
+				if (!n.getElementById(r)) {
+					var t = n.createElement("style");
+					t.id = r, c(t)
+				}
+				n.getElementById(r).innerHTML = e
+			}
+			e.FontFace && e.FontFace.prototype.hasOwnProperty("display") ? (t[r] && f(t[r]), fetch(o).then(function(e) {
+				return e.text()
+			}).then(function(e) {
+				return e.replace(/@font-face {/g, "@font-face{font-display:swap;")
+			}).then(function(e) {
+				return t[r] = e
+			}).then(f).catch(a)) : a()
+		}(window, document, localStorage);
+	</script>
+	<!-- End of code snippet for Google Fonts -->
+
 </head>
 
 <body>
