a74b0a3493f635462cd21755d56fba383e1514f6
Lê Khánh Tùng <toilatung@icloud.com>
2019-08-04 18:16:15 +0700

    About

--- src/resources/views/front/home/about.blade.php
+++ src/resources/views/front/home/about.blade.php
@@ -4,53 +4,6 @@
 @section('content')
 
 <!-- Head Section -->
-
-
-<!-- <section id="aboutv1">
-    <div class="container">
-        <div class="row">
-            <div class="decorate">
-                <img src="frontend/images/logo2.png" alt="decorate">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="ab-intro">
-                    <p>{!! $info_page_translation->description !!}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<div class="aboutflowerabv2 about floweraboutv4">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-7">
-                <div class="ab-flow-r">
-                    <div class="ab-flower-it">
-                        <h1>{{$info_page_translation->title}}</h1>
-                        <p>{!! $info_page_translation->content !!} </p>
-                    </div>
-                    <div class="ab-flower-it">
-                        <h1>Our vision</h1>
-                        <p>Pô Kô Farms determines to become a sustainable development cooperative, that helps farmers to have a better life through responsible coffee producing. Furthermore, Pô Kô Farms will be the first choice of customers who want to buy Fairtrade certified coffee in particular and agricutural  products in general from Vietnam.</p>
-                    </div>
-                    <div class="ab-flower-it">
-                        <h1>Our mission</h1>
-                        <p>Pô Kô Farms aims to become a cooperative that specializes in producing and supplying high quality Fairtrade certified coffee for current buyers and new customers in the future.
-By growing and selling Fairtrade certified coffee, Pô Kô Farms wishes to improve farmers income and quality of life for their  households .
-Sharing together and developing together, make the world better with quality coffee.
- </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <img src="frontend/images/uploads/abflow-right.png" alt="">
-            </div>
-        </div>
-    </div>
-</div> -->
-
 <div class="about aboutv2 floweraboutv1">
     <div class="container">
         <div class="row">
